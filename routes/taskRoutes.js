fff903e3474ef78d4979749042f3b658085ab637
bohdan-ly <test@gmail.com>
2023-11-04 00:27:19 +0200

    Added docker

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -7,10 +7,66 @@ const router = express.Router();
 
 router.use(checkAuth);
 
+/**
+ * @openapi
+ * '/api/task':
+ *  post:
+ *     tags:
+ *     - Task
+ *     summary: Create a task
+ *     security:
+ *      - basicAuth: []
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              description:
+ *                type: string
+ *                default: Buy a book
+ *     responses:
+ *      201:
+ *        description: Created
+ *      400:
+ *        description: Bad Request
+ */
+
 router.post('/task', taskController.createTask);
 
+/**
+ * @openapi
+ * '/api/task':
+ *  get:
+ *     tags:
+ *     - Task
+ *     summary: Get user tasks
+ *     security:
+ *       - basicAuth: []
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                type: object
+ *                properties:
+ *                  id:
+ *                    type: string
+ *                  description:
+ *                    type: string
+ *                  completed:
+ *                    type: boolean
+ *                  createdBy:
+ *                    type: string
+ *       400:
+ *         description: Bad request
+ */
 router.get('/task', taskController.getTasksByUserId);
-router.get('/task/all', checkAdmin,taskController.getAllTasks);
+router.get('/task/all', checkAdmin, taskController.getAllTasks);
 router.get('/task/:id', taskController.getTask);
 router.put('/task/:id', taskController.updateTask);
 router.delete('/task/:id', taskController.deleteTask);
